94a86fbeeb47b1c774214c96cf4521b376fc6c50
Renan Maitan <renanmaitan12@gmail.com>
2023-10-04 03:24:22 -0300

    add interaction services

---
+++ src/services/createInteraction.js
@@ -0,0 +1,36 @@
+export async function createInteraction({ interaction, authContext }) {
+    const { refreshToken, access_token, signOut } = authContext;
+
+    const IP = process.env.EXPO_PUBLIC_API_URL;
+
+    try {
+        const response = await fetch(`${IP}/api/interaction`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + access_token
+            },
+            body: JSON.stringify(interaction)
+        });
+
+
+        const status = response.status;
+
+        if (status === 201) {
+            return response;
+        } 
+        else if (status === 401) {
+            const new_token = await refreshToken();
+            if (new_token) {
+                authContext = { ...authContext, access_token: new_token}
+                return createInteraction({ interaction, authContext });
+            } else {
+                signOut();
+                return null;
+            }
+        }
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
---
+++ src/services/getInteractionsByTicket.js
@@ -0,0 +1,36 @@
+export async function getInteractionsByTicket({ ticketId, authContext }) {
+    const { refreshToken, access_token, signOut } = authContext;
+
+    const IP = process.env.EXPO_PUBLIC_API_URL;
+
+    try {
+        const response = await fetch(`${IP}/api/interaction/${ticketId}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + access_token
+            },
+        });
+
+
+        const status = response.status;
+
+        if (status === 200) {
+            json = await response.json();
+            return json;
+        } 
+        else if (status === 401) {
+            const new_token = await refreshToken();
+            if (new_token) {
+                authContext = { ...authContext, access_token: new_token}
+                return getInteractionsByTicket({ ticketId, authContext });
+            } else {
+                signOut();
+                return null;
+            }
+        }
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
